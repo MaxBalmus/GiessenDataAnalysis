57798d7b061c7be043dd5206af5a0aff7f1160fc
Maximilian Balmus <balmus.maximilian@gmail.com>
2025-05-13 10:49:01 +0100

    test

--- AnalysisGiessen/__init__.py
+++ AnalysisGiessen/__init__.py
@@ -302,10 +302,12 @@ class analyseGiessen:
         temp, temp2 = find_peaks(-self._df['fdpdt'], height=height_dpdt, distance=distance)
         self._points_df['a_epad_ind'] = temp.astype(np.int64)
         self._points_df['a_epad']     = self._df['fcPressure'][temp].values
+        self._points_df['min_dpdt']   = temp2['peak_heights'].astype(np.float64)
         
         temp, temp2 = find_peaks(self._df['fdpdt'], height=height_dpdt, distance=distance)
         self._points_df['epad_ind'] = temp.astype(np.int64)
         self._points_df['epad']     = self._df['fcPressure'][temp].values
+        self._points_df['max_dpdt'] = temp2['peak_heights'].astype(np.float64)
         
         for i, sys_ind in enumerate(self._points_df['sys_ind'].values[:-1]):
             pass
